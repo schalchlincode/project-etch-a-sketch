432ab16d3ea746bd27a56563890ec8449c249e3f
schalchlincode <schalchlincode@gmail.com>
2025-01-19 22:08:32 -0600

    feat: add 16x16 grid layout using JavaScript and CSS

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.getElementById("container");
 
 function addDiv() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 16 * 16; i++) {
     const newDiv = document.createElement("div");
 
-    newDiv.textContent = "I'm a new div!";
+    newDiv.textContent = "";
 
     container.appendChild(newDiv);
   }
